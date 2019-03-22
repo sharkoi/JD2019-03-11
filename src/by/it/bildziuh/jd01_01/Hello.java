4433074cb8a18d698f7987a769a4b57a0c096cde
Bildziuh <crazydaem@gmail.com>
2019-03-22 21:41:31 +0300

    jd01_01 done

---
+++ src/by/it/bildziuh/jd01_01/Hello.java
@@ -0,0 +1,14 @@
+package by.it.bildziuh.jd01_01;
+
+public class Hello {
+    private String slogan = "Hello world!";
+
+    void setSlogan(String slogan){
+        this.slogan = slogan;
+    }
+
+    void printSlogan(){
+        System.out.println(slogan);
+    }
+}
+
