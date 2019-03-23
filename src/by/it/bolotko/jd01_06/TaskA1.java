e21b1a2e7ade1561e6a9c7dbee5b91df59805f4c
Konstantsin Bolotko <pro100kosti@gmail.com>
2019-03-23 09:21:53 +0300

    jd01_01 (all) jd01_02 (A,B) jd01_03 (all) jd01_04 (A,B) jd01_05 (A,B) jd01_06(A1,A2)

--- src/by/it/bolotko/jd01_06/TaskA1.java
+++ src/by/it/bolotko/jd01_06/TaskA1.java
@@ -5,13 +5,13 @@ import java.util.regex.Pattern;
 
 public class TaskA1 {
     public static void main(String[] args) {
-        StringBuilder sb=new StringBuilder(Poem.text);
-        Pattern word4=Pattern.compile("[а-яА-ЯёЁ]{4,}");
-        Matcher matcher = word4.matcher(sb);
-        while (matcher.find()){
-            sb.setCharAt(matcher.start()+3,'#');
-            if (matcher.end()-matcher.start()>6)
-            sb.setCharAt(matcher.start()+6,'#');
+        StringBuilder sb=new StringBuilder(Poem.text);   //создадим КлассБилдер в котором будем размещать текст при обработке
+        Pattern word4=Pattern.compile("[а-яА-ЯёЁ]{4,}");  //регулярное выражение [все слова от а-я, от А-Я, ё, Ё] {от 4 букв до бесконечности}
+        Matcher matcher = word4.matcher(sb); //чтобы переберать слова используем матчер, он на основе выше указонного выражения будет пробегать по всем словам
+        while (matcher.find()){  //до тех пор пока Матчер что-либо находит {.......
+            sb.setCharAt(matcher.start()+3,'#'); // СетЧарЭт (в нужную позицию (от начала строки +3 индекса= 4 буква, устанавливает символ #)
+            if (matcher.end()-matcher.start()>6)     // если (индекс последней буквы в строке - индекс первой буквы = колличество элементов в строке) больше 7 элементов, то......
+            sb.setCharAt(matcher.start()+6,'#'); // СетЧарЭт (в нужную позицию (от начала строки +6 индекса= 7 буква, устанавливает символ #)
         }
         System.out.println(sb);
     }
--- src/by/it/bolotko/jd01_06/TaskA2.java
+++ src/by/it/bolotko/jd01_06/TaskA2.java
@@ -9,28 +9,34 @@ public class TaskA2 {
     private static String[] words=new String[0];
     private static int[] counters=new int[0];
 
+    private static int process(String word){
+        for (int i = 0; i <words.length; i++) {
+            if (words[i].equals(word))
+            return i;
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         StringBuilder sb=new StringBuilder(Poem.text);
         Pattern pattern=Pattern.compile("[а-яА-ЯёЁ]+");
         Matcher matcher = pattern.matcher(sb);
         while (matcher.find()){
-            String world=matcher.group();
-            process(world);
+            String word=matcher.group();
+            int p = process(word);
+            if (p>=0){
+                counters[p]++;
+            }
+            else {
+                int last=words.length; //последний элемент
+                words=Arrays.copyOf(words, last+1);
+                words[last]=word;      //запомним слово
+                counters=Arrays.copyOf(counters, last+1);
+                counters[last]=1;      //укажем частоту повторений = 1
+            }
+        }
+        for (int i = 0; i < words.length; i++) {
+            System.out.printf("%s=%d%n",words[i],counters[i]);
         }
     }
-
-    private static void process(String word){
-        for (int i = 0; i <words.length; i++) {
-            if (words[i].equals(word))
-                counters[i]++;
-            return;
-        }
-
-        words= Arrays.copyOf(words, words.length+1);
-        words [words.length-1]=word;
-
-        counters=Arrays.copyOf(counters, counters.length+1);
-        counters[words.length-1]=1;
-    }
-
 }
