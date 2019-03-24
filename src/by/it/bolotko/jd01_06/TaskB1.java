279c7252c558406b8028122540f874496bd44a5a
Konstantsin Bolotko <pro100kosti@gmail.com>
2019-03-24 20:55:55 +0300

    jd01_01 (all) jd01_02 (A,B) jd01_03 (all) jd01_04 (A,B) jd01_05 (all) jd01_06(A1,A2,B1(bed))

--- src/by/it/bolotko/jd01_06/TaskB1.java
+++ src/by/it/bolotko/jd01_06/TaskB1.java
@@ -5,13 +5,14 @@ import java.util.regex.Pattern;
 
 public class TaskB1 {
     public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder(Poem.text);
-        Pattern allWords = Pattern.compile("[а-яА-ЯёЁ]+");
-        Pattern sogl = Pattern.compile("[бвгджзйклмнпрстфхцчшщ]+");
-        Pattern glasn = Pattern.compile("[аоиеёэыуюя]+");
-        Matcher m = allWords.matcher(sb);
-        while (m.find()) {
-            String word=m.group();
+
+        String[] wordArray = Poem.text.split("[\\s,.:!?]+");
+        Pattern pattern = Pattern.compile("^[бвгджзйклмнпрстфхцчшщБВГДЖЗЙКЛМНПРСТФХЦЧШЩ].*[аоэиуыеёюяАОЭИУЫЕЁЮЯ]$");
+        for (String word : wordArray) {
+            Matcher matcher = pattern.matcher(word);
+            if (matcher.find()) {
+                System.out.println(word);
+            }
         }
     }
 }
