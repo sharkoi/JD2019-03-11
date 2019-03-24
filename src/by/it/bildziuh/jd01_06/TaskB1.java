636567cc58b4e77c5b2f2c19f2fe450c251194c4
Bildziuh <crazydaem@gmail.com>
2019-03-24 18:02:55 +0300

    jd01_06 Task B1 - Избушка?

--- src/by/it/bildziuh/jd01_06/TaskB1.java
+++ src/by/it/bildziuh/jd01_06/TaskB1.java
@@ -1,5 +1,30 @@
 package by.it.bildziuh.jd01_06;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class TaskB1 {
 
+    public static void main(String[] args) {
+        Pattern pattern = Pattern.compile("[а-яА-ЯёЁ]{3,}");
+        Matcher matcher = pattern.matcher(Poem.text);
+        while (matcher.find()) {
+            String word = matcher.group();
+            compare(word);
+
+        }
+    }
+
+    private static void compare(String word) {
+        char[] vowels = {'а', 'я', 'о', 'ё', 'ы', 'и', 'э', 'е', 'у', 'ю',
+                         'А', 'Я', 'О', 'Ё', 'Ы', 'И', 'Э', 'Е', 'У', 'Ю'};
+
+        for (int i = 0; i < vowels.length; i++) {
+            if (word.charAt(0) != vowels[i]) {
+                if (word.charAt(word.length() - 1) == vowels[i])
+
+                    System.out.println(word);
+            }
+        }
+    }
 }
