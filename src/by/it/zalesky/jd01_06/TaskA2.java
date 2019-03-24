b4061d9d0fdcd9aeaa1964656466533190179163
Alexander Khmelov <akhmelev@gmail.com>
2019-03-24 23:26:42 +0300

    jd01_07

--- src/by/it/zalesky/jd01_06/TaskA2.java
+++ src/by/it/zalesky/jd01_06/TaskA2.java
@@ -4,20 +4,20 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TaskA2 {
-    private static int[] counters=new int[];
+    private static int[] counters=new int[0];
     private static String[] words = new String[0];
     
     public static void main(String[] args) {
-        Pattern pattern= Pattern.compile("[a-яА-ЯёЁ] {4,}");
-        Matcher matcher = new StringBuilder(Poem.text);
-        while (matcher.find()){
-            String word = matcher.group();
-            process(word);
-           
-        }
-        for (int i = 0; i < words.length; i++) {
-            
-        }
-    }
+//        Pattern pattern= Pattern.compile("[a-яА-ЯёЁ] {4,}");
+//        Matcher matcher = new StringBuilder(Poem.text);
+//        while (matcher.find()){
+//            String word = matcher.group();
+//            process(word);
+//
+//        }
+//        for (int i = 0; i < words.length; i++) {
+//
+//        }
+//    }
 }
 }
